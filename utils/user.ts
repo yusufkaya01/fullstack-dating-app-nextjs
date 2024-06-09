afbb06224db9373bf3b551bb69e5b379d575f5a2
tanvir07-cell <tr9836859@gmail.com>
2024-06-09 02:06:53 -0700

    Chore:For the caching the dynamic request getUser coookies

--- utils/user.ts
+++ utils/user.ts
@@ -2,12 +2,14 @@
 import { cookies } from "next/headers";
 import { COOKIE_NAME } from "./constants";
 import { getUserFromToken } from "./authDb";
+import { cache } from "react";
 
-export const getUser = async () => {
+export const getUser = cache(async () => {
+  console.log("user");
   const userTokenfromCookie = cookies().get(COOKIE_NAME) as {
     name: string;
     value: string;
   };
   const user = await getUserFromToken(userTokenfromCookie);
   return user;
-};
+});
